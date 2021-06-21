09712df3baddebcff57ffcb93c877c678a5ee462
Gonzalo Robles <grobles@blueminds.cl>
2021-06-21 10:59:22 -0400

    Delete: error in uploading

--- app_procurement_plan/models/purchase_order.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models, api, _
-from datetime import datetime, time
-
-class PurchaseOrder(models.Model):
-    _inherit = "purchase.order"
-
-    proc_plan_id = fields.Many2one('procurement.plan', 'Procurement Plan', readonly=True, copy=False)
