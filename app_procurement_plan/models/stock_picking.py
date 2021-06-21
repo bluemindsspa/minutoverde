364956a83b76ec0455514b303a9a54fcacabf0ef
Gonzalo Robles <grobles@blueminds.cl>
2021-06-21 11:00:06 -0400

    Delete: error in uploading

--- app_procurement_plan/models/stock_picking.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    proc_plan_id = fields.Many2one('procurement.plan', 'Procurement Plan', readonly=True)
-
