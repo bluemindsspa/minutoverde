994fa1699b2b7e68aa6ba5be28833d180a4bcce8
yayac05 <jcarrasquero@blueminds.cl>
2021-07-14 22:44:01 -0400

    Joyza. Creación de los campos familia, agrupación familia, sub familia, etc para productos

--- custom_minutoverde/models/product_template.py
+++ custom_minutoverde/models/product_template.py
@@ -7,9 +7,25 @@ class ProductTemplate(models.Model):
     _description = "Product Template"
 
     family_id = fields.Many2one('product.family', string='Agrupación Familia')
-
+    fam_id = fields.Many2one('product.fam', string="Familia")
+    sub_fam_id = fields.Many2one('product.fam', string="Sub Familia")
+    categ_fam_id = fields.Many2one('product.categoria.fam', string="Categoría")
+    sub_categ_fam_id = fields.Many2one('product.categoria.fam', string="Sub Categoría")
+    
 class ProductFamily(models.Model):
     _name = 'product.family'
     _description = 'Agrupación Familiar'
+   
+    name = fields.Char(string='Nombre')
+
+class ProductFam(models.Model):
+    _name = 'product.fam'
+    _description = 'Familia'
+   
+    name = fields.Char(string='Nombre')
+
+class ProductCategFam(models.Model):
+    _name = 'product.categoria.fam'
+    _description = 'Categoría'
    
     name = fields.Char(string='Nombre')
